08131f716f24d681a81bba7833ad0fc8ae5b938d
Andrew Ge <ge.andrew@gmail.com>
2024-11-09 22:34:37 -0800

    loading and login page skeleton

---
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.fitnesstracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gotoLoginButton = findViewById(R.id.goto_login_button);
+        gotoLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
